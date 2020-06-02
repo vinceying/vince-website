81fe838834a82e0952916fd9b8fd7e0c6e94236d
vinceying <admin@vince.pub>
2020-06-02 17:50:02 +0800

    Update CDN links

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,9 +67,9 @@
 
             // Images (in the format of 'url': 'alignment').
             images: {
-                'images/bg01.jpg': 'center',
-                'images/bg02.jpg': 'center',
-                'images/bg03.png': 'center'
+                'https://i.vince.pub/website/images/bg01.jpg': 'center',
+                'https://i.vince.pub/website/images/bg02.jpg': 'center',
+                'https://i.vince.pub/website/images/bg03.png': 'center'
             },
 
             // Delay.
